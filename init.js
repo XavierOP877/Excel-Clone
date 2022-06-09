e0b4d60898af6f331365bf4e887e5c69888c198e
XavierOP877 <tshivank620@gmail.com>
2022-06-09 22:05:07 +0530

    excel completed

--- init.js
+++ init.js
@@ -31,22 +31,34 @@ function initCells(){
 
 initCells();
 
+let sheetsDB = [];
 let db;
+let visitedCells;
 
 function initDb(){
-    db = [];
+
+    let newSheetDB = [];
     for(let i=0;i<100;i++){
         let row = [];
         for(let j=0;j<26;j++){
             let name = String.fromCharCode(65+j)+(i+1)+"";
             let cellObject = {
                 name:name,
-                value:""
+                value:"",
+                formula:"",
+                children:[],
+                parent:[],
+                visited:false,
+                fontStyle:{bold:false,italic:false,underline:false}
             }
             row.push(cellObject);
         }
-        db.push(row);
+        newSheetDB.push(row);
     }
+    visitedCells = [];
+    db = newSheetDB;
+    sheetsDB.push({db:newSheetDB,visitedCells:visitedCells});
+    console.log(sheetsDB);
 }
 initDb();
 
